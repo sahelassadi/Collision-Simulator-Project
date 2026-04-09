4850ff07726025d46ba622d273e5fbf44f5e747a
sahelassadi <sahel552010@hotmail.com>
2026-04-08 20:17:51 -0400

    Delete CollisionSimulator directory

--- CollisionSimulator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.demo1234 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    exports org.example.collisionsimulator;
-    opens org.example.collisionsimulator to javafx.fxml;
-}
